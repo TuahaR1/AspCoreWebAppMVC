c39eb5a83c396499a6203e1f17cf167513c076ef
Tuaha Rasool <92690142+TuahaR1@users.noreply.github.com>
2024-10-07 19:30:20 +0500

    intendation issue resolve

--- AspCoreWebAppMVC/Controllers/HomeController.cs
+++ AspCoreWebAppMVC/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace AspCoreWebAppMVC.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+
         }
 
         public IActionResult Index()
